68a03425b40777666cc9b13218f5b3c8c0ded8f1
Carlos Barahona <developer@carlosb.xyz>
2023-05-16 21:19:31 -0500

    Setting the log in

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,7 +3,7 @@ import  jwt  from "jsonwebtoken";
 import User from "../models/User.js";
 
 /*Register user */
-export const register = async (req, res)=>{
+export const register = async (req, res) => {
 try {
     const{
         firstName,
@@ -38,3 +38,25 @@ try {
     res.status(500).json({error: err.message});
  }
 };
+
+/* Log in */
+
+export const login = async (req, res) => {
+    try {
+        const {email, password } = req.body;
+        const user = await User.findOne({email: email});
+        if(!user) return res.status(400).json({msg :"User does not exist."});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({msg :"Invalid credentials."});
+
+        const token =  jwt.sign({id: user._id}, process.env.JWT_SECRET);
+
+        delete user.password;
+
+        res.status(200).json({token, user});
+
+    } catch (err) {
+        res.status(500).json({error: err.message});
+     }
+}
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js"
 import {register} from "./controllers/auth.js"
 
 /*CONFIGURATIONS(Middleware)*/
@@ -38,6 +39,9 @@ const upload = multer({ storage })
 /*ROUTES WITH FILES */
 
 app.post("/auto/register", upload.single("picture"), register);
+
+/*Routes*/
+app.use("/auth", authRoutes);
 
 /* MONGOOSE SETUP */
 
