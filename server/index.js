fd82ad0232b955b6592e24eb2bc43662526b0d34
Carlos Barahona <developer@carlosb.xyz>
2023-05-17 21:01:53 -0500

    Adding posts  /dummy data-backend is finished

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import  express  from "express";
+import express  from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -10,7 +10,13 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
 import {register} from "./controllers/auth.js"
+import {createPost} from "./controllers/posts.js"
+import { verifyToken } from "./middleware/auth.js";
+/*import User from "./models/User.js"
+import Post from "./models/posts.js";
+import {users, posts} from "./data/index.js"*/
 
 /*CONFIGURATIONS(Middleware)*/
 const __filename = fileURLToPath(import.meta.url);
@@ -38,12 +44,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 /*ROUTES WITH FILES */
-
 app.post("/auto/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /*Routes*/
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 
@@ -53,4 +60,9 @@ mongoose.connect(process.env.MONGO_URL,{
     useUnifiedTopology: true,
 }).then(()=>{
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* Keep this code commented, it adds dummy data if needed 
+    User.insertMany(users);
+    Post.insertMany(posts);*/
+
 }).catch((error) => console.log(`${error} did not connect`));
