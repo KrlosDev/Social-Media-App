e9fd22982b79a6399fb4c99f110c75cf1dd3ff7b
Carlos Barahona <developer@carlosb.xyz>
2023-06-19 21:12:50 -0500

    folder architecture/routers and redux set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "scenes/home";
+import LoginPage from "scenes/login";
+import ProfilePage from "scenes/profile";
 
 function App() {
   return (
     <div className="app">
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
